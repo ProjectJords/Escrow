500e84656dac8559d78ebc8bd0199fb2d7c66c95
Projectjords <jords@Ra.localdomain>
2023-01-08 21:41:21 -0500

    updated to deploy Lease contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Escrow = artifacts.require("Escrow");
+const Lease = artifacts.require("Lease");
 
 module.exports = function (deployer) {
   deployer.deploy(Escrow);
+  deployer.deploy(Lease);
 };
